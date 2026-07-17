31f4a5c6fae7fd90ccd807248980ec96b31f248d
Helldez <74499579+Helldez@users.noreply.github.com>
2026-07-17 18:55:33 +0200

    chore(format): reflow csv_metrics_sink header printf to clang-format

--- core/src/metrics/csv_metrics_sink.cpp
+++ core/src/metrics/csv_metrics_sink.cpp
@@ -24,8 +24,8 @@ public:
         std::fprintf(f_,
                      "# moe_stream=%d cache_mb=%d cache_auto=%d cache_ceil_mb=%d force_cache=%d "
                      "io_threads=%d o_direct=%d overlap=%d prefetch=%d dense_weights=%s\n",
-                     r.moe_stream, r.cache_mb, r.cache_auto, r.cache_ceil_mb, r.force_cache,
-                     r.io_threads, r.o_direct, r.overlap, r.prefetch_layers, r.dense_weights.c_str());
+                     r.moe_stream, r.cache_mb, r.cache_auto, r.cache_ceil_mb, r.force_cache, r.io_threads, r.o_direct,
+                     r.overlap, r.prefetch_layers, r.dense_weights.c_str());
         write_header();
     }
 
